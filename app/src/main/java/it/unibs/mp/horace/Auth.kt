b3b444f5f1220667112a69726ccb718af1d09de5
H3isenb3rg <matteoe97@gmail.com>
2023-05-29 15:07:10 +0200

    Partial commit for merge

--- app/src/main/java/it/unibs/mp/horace/Auth.kt
+++ app/src/main/java/it/unibs/mp/horace/Auth.kt
@@ -2,18 +2,29 @@ package it.unibs.mp.horace
 
 import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebView.FindListener
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.MaterialToolbar
 import it.unibs.mp.horace.databinding.AuthBinding
 
 class Auth : AppCompatActivity() {
     private lateinit var binding: AuthBinding
+    private lateinit var topBar: MaterialToolbar
     // private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = AuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Top App Bar configs
+        binding.appBarLayout.topAppBar.title = getString(R.string.action_sign_in)
+        binding.appBarLayout.topAppBar.setNavigationOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
