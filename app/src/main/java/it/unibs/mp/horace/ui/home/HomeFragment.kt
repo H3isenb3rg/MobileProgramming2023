16edbe82737638907d4dcf2d536f8a79785c0fcd
Luca Cotti <lucacotti@outlook.com>
2023-06-05 23:10:01 +0200

    Adds snackbar on successful sign up

--- app/src/main/java/it/unibs/mp/horace/ui/home/HomeFragment.kt
+++ app/src/main/java/it/unibs/mp/horace/ui/home/HomeFragment.kt
@@ -4,18 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
+import it.unibs.mp.horace.R
 import it.unibs.mp.horace.TopLevelFragment
 import it.unibs.mp.horace.databinding.FragmentHomeBinding
 
 class HomeFragment : TopLevelFragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private val args: HomeFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        if (args.signUpSuccessful) {
+            Snackbar.make(
+                view, getString(R.string.signed_up_successfully), Snackbar.LENGTH_SHORT
+            ).show()
+        }
     }
 
     override fun onDestroyView() {
