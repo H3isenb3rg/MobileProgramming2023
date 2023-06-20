f3da2a24bf0c4a1d9f2985490ea43e4706bb53f4
Luca Cotti <lucacotti@outlook.com>
2023-06-20 19:15:11 +0200

    Moves stuff around, fixes bugs

--- app/src/main/java/it/unibs/mp/horace/ui/settings/UpdateProfileFragment.kt
+++
@@ -1,7 +0,0 @@
-package it.unibs.mp.horace.ui.settings
-
-import androidx.fragment.app.Fragment
-
-class UpdateProfileFragment: Fragment() {
-    // TODO: Add navigation in nav_graph
-}
---
+++ app/src/main/java/it/unibs/mp/horace/ui/updateprofile/UpdateProfileFragment.kt
@@ -0,0 +1,6 @@
+package it.unibs.mp.horace.ui.updateprofile
+
+import androidx.fragment.app.Fragment
+
+class UpdateProfileFragment : Fragment() {
+}
