ad42749da1c7ad280cce4a1aa57186c7fe24922c
Matteo Beatrice <matteoe97@gmail.com>
2023-06-24 18:58:55 +0200

    Profile picture edit and slight changes to LoggedUser Setters

--- app/src/main/java/it/unibs/mp/horace/backend/TimeEntry.kt
+++ app/src/main/java/it/unibs/mp/horace/backend/TimeEntry.kt
@@ -2,22 +2,13 @@ package it.unibs.mp.horace.backend
 
 import java.time.Instant
 
-class TimeEntry {
+data class TimeEntry(
+    val idActivity: String,
+    val description: String,
+    val isPomodoro: Boolean,
+    val startTime: String,
+    val endTime: String
+    ) {
     var uid: String? = null
-    var idActivity: String? = null
-    var description: String? = null
-
     // FIXME: Currently date times are strings we need to find the correct type to use
-    var startTime: String? = null
-    var endTime: String? = null
-
-    constructor()
-
-    constructor(uid: String?, idActivity: String?, description: String?, startTime: String?, endTime: String?) {
-        this.uid = uid
-        this.idActivity = idActivity
-        this.description = description
-        this.startTime = startTime
-        this.endTime = endTime
-    }
 }
--- app/src/main/java/it/unibs/mp/horace/backend/User.kt
+++ app/src/main/java/it/unibs/mp/horace/backend/User.kt
@@ -1,10 +1,25 @@
 package it.unibs.mp.horace.backend
 
-data class User(var name: String, var email: String, var uid: String) {
+import android.net.Uri
+import java.net.URI
+
+data class User(var name: String?, var email: String, var uid: String, var photoURI: Uri? = null) {
     companion object {
         const val COLLECTION_NAME: String = "users"
         const val EMAIL_FIELD: String = "email"
         const val USERNAME_FIELD: String = "username"
+        const val PHOTO_FIELD: String = "photo_uri"
         const val UID_FIELD: String = "uid"
+    }
+
+    fun toHashMap(): HashMap<String, String> {
+        val map = hashMapOf(
+            EMAIL_FIELD to email,
+            UID_FIELD to uid
+        )
+        name?.let { map[USERNAME_FIELD] = it }
+        photoURI?.let { map[PHOTO_FIELD] = it.toString() }
+
+        return map
     }
 }
